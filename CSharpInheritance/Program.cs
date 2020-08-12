59fedd0b0b0a6bff5150f4044092e57e17de5b09
Joey Petrone <joey.petrone@gmail.com>
2020-08-12 18:30:52 -0500

    Add project files.

---
+++ CSharpInheritance/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
