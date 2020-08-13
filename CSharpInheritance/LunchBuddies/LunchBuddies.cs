0b351642ec7efb4506e08536ca0a3eac32a36fe0
Joey Petrone <joey.petrone@gmail.com>
2020-08-12 21:02:11 -0500

    Lunch Buddies app complete

--- CSharpInheritance/LunchBuddies/LunchBuddies.cs
+++ CSharpInheritance/LunchBuddies/LunchBuddies.cs
@@ -10,7 +10,17 @@ namespace CSharpInheritance.LunchBuddies
         {
             Console.WriteLine("In Lunch Buddies app.");
 
-            var restaurant = new Restaurant();
+            var lunchBuddy = new LunchBuddy("Jenny", "Chu");
+            var hungyBuddy = new LunchBuddy("John", "Doe");
+            var sleepyBuddy = new LunchBuddy("Henry", "Flop");
+            var skinnyBuddy = new LunchBuddy("Larry", "Sqomp");
+
+            var lunchCompanions = new List<LunchBuddy> { hungyBuddy, sleepyBuddy, skinnyBuddy};
+
+            lunchBuddy.eat();
+            lunchBuddy.eat("sandwhich");
+            lunchBuddy.eat(lunchCompanions);
+            lunchBuddy.eat("another sandwhich", lunchCompanions);
         }
     }
 }
