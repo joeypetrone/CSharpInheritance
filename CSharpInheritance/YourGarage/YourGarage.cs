fe5a79e94e257848742d42e4b14a5719515e4884
Joey Petrone <joey.petrone@gmail.com>
2020-08-15 09:43:37 -0500

    Car and Vehicle added

---
+++ CSharpInheritance/YourGarage/YourGarage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpInheritance.YourGarage
+{
+    class YourGarage
+    {
+        public void Run()
+        {
+            Console.WriteLine("In Your Garage app.");
+
+            // Build a collection of all vehicles that fly
+            // With a single `foreach`, have each vehicle Fly()
+
+            // Build a collection of all vehicles that operate on roads
+            // With a single `foreach`, have each road vehicle Drive()
+
+            // Build a collection of all vehicles that operate on water
+            // With a single `foreach`, have each water vehicle Drive()
+        }
+    }
+}
